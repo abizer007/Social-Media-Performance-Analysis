1794b94022bb26786d77963597055de5db5c26c2
Abizer Masavi <133958757+abizer007@users.noreply.github.com>
2025-01-08 22:07:10 +0530

    Update and rename server.py to app.py

--- app.py
+++ app.py
@@ -179,4 +179,5 @@ def query_database():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
